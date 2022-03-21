10d04467991354123bc843ed8bb60c1fdf520178
PoojaSolankar <poojasolankar09gmail.com>
2022-03-21 19:08:47 +0530

    Append Linked List

--- linkedlistproblem/LinkedList.java
+++ linkedlistproblem/LinkedList.java
@@ -5,21 +5,34 @@ public class LinkedList {
 	Node head;
 	Node tail;
 	
-	public void add(int i) {
-		Node newNode = new Node(i);
+	public void add(int data) {
+		Node newNode = new Node(data);
 		if(head == null) {
 		head = newNode;
 		tail = newNode;
 		}
-		else
-		{
-			head.next = newNode;
+		else{
+			tail.next = newNode;
 			tail = newNode;
-			
 		}
-		
-	//public void show() {
-			
-		
+	}	
+	
+	public void append( int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+		head = newNode;
+		tail = newNode;
+		}
+		else {
+		    tail.next = newNode;
+			tail = newNode;
+		}
+	}
+	public void show() {
+		Node temp = head;
+		while (temp != null) {
+		System.out.print(temp.data + " -->");
+		temp = temp.next;
+		}
 	}
 }
--- linkedlistproblem/MainClass.java
+++ linkedlistproblem/MainClass.java
@@ -4,12 +4,15 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		System.out.println(" Welcome to LinkedList Program");
-		java.util.LinkedList linkedlist = new java.util.LinkedList ();
-	
-		linkedlist.add(70);
-		linkedlist.add(30);
-		linkedlist.add(56);
-		System.out.println(linkedlist);
+		//java.util.LinkedList list = new java.util.LinkedList ();
+		
+	    LinkedList linkedlist = new LinkedList();
+	    
+	    linkedlist.append(56);
+		linkedlist.append(30);
+	    linkedlist.append(70);
+		   
+		linkedlist.show();
+		System.out.println();
 	}
-
 }
