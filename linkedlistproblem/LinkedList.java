5abcf4d7584e03b2e21667f9d0566dec63a0d4b3
PoojaSolankar <poojasolankar09gmail.com>
2022-03-24 16:59:01 +0530

    Insert Between

--- linkedlistproblem/LinkedList.java
+++ linkedlistproblem/LinkedList.java
@@ -15,9 +15,9 @@ public class LinkedList {
 			tail.next = newNode;
 			tail = newNode;
 		}
-	}	
-	
+	}
 	public void append( int data) {
+		
 		Node newNode = new Node(data);
 		if(head == null) {
 		head = newNode;
@@ -28,6 +28,38 @@ public class LinkedList {
 			tail = newNode;
 		}
 	}
+	
+	public void insertAtMiddle(int data, int position) {
+		Node temp;
+		int i;
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+				newNode.data = data; 
+		        newNode.next = null;
+		        temp = head;
+
+		        for(i=2; i<=position-1; i++)
+		        {
+		            temp = temp.next;
+
+		            if(temp == null)
+		                break;
+		        }
+
+		        if(temp != null) {
+		            newNode.next = temp.next; 
+		            temp.next = newNode;
+		            System.out.println("Data Inserted Successfully\n");
+		        }
+		        else {
+		            System.out.println("Can Not Insert Data At The Given Position\n");
+		        }
+			}
+		}
+	
 	public void show() {
 		Node temp = head;
 		while (temp != null) {
@@ -36,3 +68,6 @@ public class LinkedList {
 		}
 	}
 }
+	
+
+
--- linkedlistproblem/MainClass.java
+++ linkedlistproblem/MainClass.java
@@ -4,15 +4,17 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		System.out.println(" Welcome to LinkedList Program");
-		//java.util.LinkedList list = new java.util.LinkedList ();
 		
 	    LinkedList linkedlist = new LinkedList();
+	    linkedlist.add(56);
+	    linkedlist.add(70);
+	   
+	    linkedlist.show();
+	    System.out.println();
 	    
-	    linkedlist.append(56);
-		linkedlist.append(30);
-	    linkedlist.append(70);
-		   
-		linkedlist.show();
-		System.out.println();
+	    linkedlist.insertAtMiddle(30, 2);
+	    linkedlist.show();
+	    System.out.println();
+	    
 	}
 }
